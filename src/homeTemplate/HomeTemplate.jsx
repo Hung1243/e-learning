9af60e56a495ca5e20ecd7fac5730084d644dca5
An Tran <tranvinhthuyan9797@gmail.com>
2024-03-02 13:10:31 +0700

    update viewcourse

--- src/homeTemplate/HomeTemplate.jsx
+++ src/homeTemplate/HomeTemplate.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ViewCourse from "../pages/user/ViewCourse";
+import { Outlet } from "react-router-dom";
 
 const HomeTemplate = () => {
-  return <div>HomeTemplate</div>;
+  return (
+    <div>
+      HomeTemplate
+      <Outlet />
+    </div>
+  );
 };
 
 export default HomeTemplate;
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ViewCourse from "./ViewCourse";
 
 const Home = () => {
   return <div>Home</div>;
