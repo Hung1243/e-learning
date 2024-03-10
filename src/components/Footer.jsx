6f145530e6943bff6ad3e0ce7e912a4b8c533a04
LeThanhHung <thanhhung1243it@gmail.com>
2024-03-10 17:09:10 +0700

    home-page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container padding">
+          <div className="box logo">
+            <h1>E-LEARNING</h1>
+            <span>ONLINE EDUCATION & LEARNING</span>
+            <p>
+              A small river named Duden flows by their place and supplies it
+              with the necessary regelialia.
+            </p>
+
+            <i className="fab fa-facebook-f icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-instagram icon"></i>
+          </div>
+          <div className="box link">
+            <h3>Explore</h3>
+            <ul>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Courses</li>
+              <li>Blog</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div className="box link">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Contact Us</li>
+              <li>Pricing</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy</li>
+              <li>Feedbacks</li>
+            </ul>
+          </div>
+
+          <div className="box last">
+            <h3>Have a Questions?</h3>
+            <ul>
+              <li>
+                <i className="fa fa-map"></i>2 Bis Nguyen Thi Minh Khai, Distric
+                1, Ho Chi Minh City
+              </li>
+              <li>
+                <i className="fa fa-phone-alt"></i>
+                +2 392 3929 210
+              </li>
+              <li>
+                <i className="fa fa-paper-plane"></i>
+                elearning@cybersoft.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      <div className="legal">
+        <p>
+          Copyright ©2024 All rights reserved | This website made by An and Hung
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/homeTemplate/HomeTemplate.jsx
+++ src/homeTemplate/HomeTemplate.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const HomeTemplate = () => {
-  return <div>HomeTemplate</div>;
+  return (
+    <div>
+      <Header />
+
+      <div className="content" style={{ minHeight: "90vh" }}>
+        <Outlet />
+      </div>
+      <footer className="p-3 text-center bg-black">
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default HomeTemplate;
